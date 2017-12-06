3ca28ef42a56b0a8fcbd45bb311a79abfc6fb15f
TonyWhiteb <tonybai2016@gmail.com>
2017-12-06 14:04:18 -0600

    Create OnRightDown and sub functions

--- FileDropCtrl.py
+++ FileDropCtrl.py
@@ -9,6 +9,9 @@ class FileListCtrl(wx.ListCtrl):
         self.currRow = None
 
         self.Bind(wx.EVT_LEFT_DOWN, self.OnFindCurrentRow )
+
+        self.entriesList = []
+        self.numEntries = 0
         # self.Bind(wx.EVT_RIGHT_DOWN,)
 
     def OnFindCurrentRow(self,event): #find current row control
@@ -19,17 +22,45 @@ class FileListCtrl(wx.ListCtrl):
             # HitTest, Determine which item is at the specified point.
             # Returns index of the item or wxNOT_FOUND if no item is at the specified point.
         self.currRow = row
-        self.Select(row)    
+        self.Select(row)
 
-    # def OnRightDown(self,event): #Right click menu
-    #
-    #     menu = wx.Menu()
-    #     menuItem = menu.Append(-1,'Delete this file')
-    #
-    #     self.Bind(wx.EVT_MENU, )
+    def OnRightDown(self,event): #Right click menu
 
+        menu = wx.Menu()
+        menuItem = menu.Append(-1,'Delete this file')
 
+        self.Bind(wx.EVT_MENU, )
 
+    def OnDeleteRow(self, event):
+
+        if(self.currRow >= 0):
+
+            assert(self.numEntries == len(self.entriesList))
+
+            allSelectedRowData = self.GetAllSelectedRowData()
+
+    def GetAllSelectedRowData(self):
+        allSelectedRowData = []
+        idx = -1
+        while True:
+            idx = self.GetNextItem(idx,wx.LIST_NEXT_ALL, wx.LIST_STATE_SELECTED)
+            #Searches for an item with the given geometry or state,starting from item but excluding the item itself
+            #if item is -1, the first item that matches the specified flags will be returned.
+            #Return the first item with given state following item or -1 if no such item found.
+            if (idx == -1):
+                break
+
+            allSelectedRowData.append( self.GetItemInfo(idx))
+    def GetItemInfo(self,idx):
+        rowItemList = []
+        rowItemList.append(idx)
+        rowItemList.append(self.GetItemData(idx)) #Gets the application-defined data associated with this item
+        rowItemList.append(self.GetItemText(idx)) #gets the item text for this item, Column 0 is the default
+
+        for i in range(1,self.GetColumnCount()):
+            rowItemList.append(self.GetItem(idx, i).GetText())
+
+        return rowItemList
 
 class FileDropCtrl(wx.Panel):
 
