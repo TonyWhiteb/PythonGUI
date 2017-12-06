995db324dd11fb1898d968cea28ef36ec3eb2c19
TonyWhiteb <tonybai2016@gmail.com>
2017-12-06 10:35:33 -0600

    Create find current row control

--- FileDropCtrl.py
+++ FileDropCtrl.py
@@ -1,10 +1,44 @@
 import sys,os
 import wx
 
+
+class FileListCtrl(wx.ListCtrl):
+    def __init__(self,*args,**kwargs):
+        super(FileListCtrl,self).__init__(*args,**kwargs)
+
+        self.currRow = None
+
+        self.Bind(wx.EVT_LEFT_DOWN, self.OnFindCurrentRow )
+        # self.Bind(wx.EVT_RIGHT_DOWN,)
+
+    def OnFindCurrentRow(self,event): #find current row control
+        if (self.currRow is not None):
+            self.Select(self.currRow, False)
+
+        row,_ignoredFlags = self.HitTest(event.GetPosition())
+            # HitTest, Determine which item is at the specified point.
+            # Returns index of the item or wxNOT_FOUND if no item is at the specified point.
+        self.currRow = row
+        self.Select(row)    
+
+    # def OnRightDown(self,event): #Right click menu
+    #
+    #     menu = wx.Menu()
+    #     menuItem = menu.Append(-1,'Delete this file')
+    #
+    #     self.Bind(wx.EVT_MENU, )
+
+
+
+
 class FileDropCtrl(wx.Panel):
 
     def __init__(self, parent,size = (-1,100), label= 'default title', DEVEL =False ):
 
         super( FileDropCtrl,self).__init__(parent = parent, id =-1,style= wx.SIMPLE_BORDER)
 
-        fdcLabel = wx.StaticText(self,-1,label = ' '+ label, size = (-1,17))
+        fdcLabel = wx.StaticText(self,-1,label = ' '+ label, size = (-1,17))#Creating and Showing a text control
+
+        SetBold(fdcLabel)
+
+        fdcID = wx.NewId()
